c1374ac6e20bbaff9a147cf5d547f5af00258933
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-05 15:21:50 +0200

    Creation of the DbEntities

---
+++ Infrastructure/Ef/DbEntities/DbTrip.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure.Ef.DbEntities;
+
+public class DbTrip
+{
+    public int Id { get; set; }
+    public int IdDriver { get; set; }
+    public bool Smoke { get; set; }
+    public double Price { get; set; }
+    public bool Luggage { get; set; }
+    public bool PetFriendly { get; set; }
+    public DateTime Date { get; set; }
+    public string DriverMessage { get; set; }
+    public bool AirConditioning { get; set; }
+    public int CityStartingPoint  { get; set; }
+    public int CityDestination  { get; set; }
+    public string PlateNumber  { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+}
---
+++ Infrastructure/Ef/DbEntities/DbUser.cs
@@ -0,0 +1,17 @@
+﻿namespace Infrastructure.Ef.DbEntities;
+
+public class DbUser
+{
+    public int Id { get; set; }
+    public string UserType { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public DateTime BirthDate { get; set; }
+    public bool IsBanned { get; set; }
+    public string PhoneNumber { get; set; }
+    public string? LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? Gender { get; set; }
+    public string City { get; set; }
+}
