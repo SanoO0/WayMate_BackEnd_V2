302ac59ece4d045178143c3cd704ee61647d2ea1
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-05 14:42:21 +0200

    Automatic change

--- Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net6.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+707e921934c4b081a67f77c43f1d6bb7d1ce2933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadccb850c8971fe1b7c68b642789113bcecf0cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
