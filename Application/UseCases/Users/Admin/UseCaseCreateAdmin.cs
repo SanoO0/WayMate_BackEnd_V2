7c568d8783f9ba556e4b7d37c3dbda7a39ad61d3
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-05 23:06:28 +0200

    Created UseCase for Admin

---
+++ Application/UseCases/Users/Admin/UseCaseCreateAdmin.cs
@@ -0,0 +1,23 @@
+﻿using Application.UseCases.Users.Admin.Dto;
+using AutoMapper;
+using Infrastructure.Ef.Users.Admin;
+
+namespace Application.UseCases.Users.Admin;
+
+public class UseCaseCreateAdmin
+{
+    private readonly IAdminRepository _adminRepository;
+    private readonly IMapper _mapper;
+
+    public UseCaseCreateAdmin(IMapper mapper, IAdminRepository adminRepository)
+    {
+        _mapper = mapper;
+        _adminRepository = adminRepository;
+    }
+
+    public DtoOutputAdmin Execute(DtoInputCreateAdmin input)
+    {
+        var dbUser = _adminRepository.CreateAdmin(input.Username, input.Password, input.Email, input.Birthdate, input.IsBanned, input.PhoneNumber);
+        return _mapper.Map<DtoOutputAdmin>(dbUser);
+    }
+}
