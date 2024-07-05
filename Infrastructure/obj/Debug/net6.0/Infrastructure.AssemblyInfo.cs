eb947255b37450480862e63f12e637d8c53c9de7
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-05 15:36:12 +0200

    Addition of nuggets necessary

--- Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net6.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fadccb850c8971fe1b7c68b642789113bcecf0cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1374ac6e20bbaff9a147cf5d547f5af00258933")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
