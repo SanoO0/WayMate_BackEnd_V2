04ec7d208dee79af3eb52ec9ca39f6a2bd159aca
SanoO0 <mathiasmanzano37@gmail.com>
2024-07-05 23:52:30 +0200

    Created Dtos and UseCase for Authentication

---
+++ Application/UseCases/Authentication/Dtos/DtoOutputLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.UseCases.Authentication.Dtos;
+
+public class DtoOutputLogin
+{
+    public bool isLogged { get; set; }
+    public string username { get; set; }
+    public string usertype { get; set; }
+}
